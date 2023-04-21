1500a8912d45d94090f019853e5402cb5442795f
Zihan Miao <126645493+purplesweat@users.noreply.github.com>
2023-04-21 11:27:18 -0700

    Update install.sh to work without direct sudo

--- install.sh
+++ install.sh
@@ -5,8 +5,18 @@ then
     # We have cargo, that is good
     cargo build --release
     mkdir -p /usr/share/dict
-    cp assets/words.sorted /usr/share/dict
-    cp target/release/wordle ~/.cargo/bin
+    su_cmd=
+    if command -v sudo
+    then
+        su_cmd=sudo
+    else if command -v doas
+    then
+        su_cmd=doas
+    else
+        su_cmd=su -c "$@"
+    fi
+    $su_cmd cp assets/words.sorted /usr/share/dict/words.sorted
+    $su_cmd cp target/release/wordle-rs $HOME/.cargo/bin/wordle-rs
 else
     # I have not an idea how to compile the random crate...
     echo "cargo doesn't seem to be installed, try"
